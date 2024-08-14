b7ca18fe6c4756caccb023b42b98ada8bb2ea311
Meine liebe <ephemeralshift@yandex.ru>
2024-08-14 21:43:37 +0400

    scripts

---
+++ Scripts/UC07_userSettle/data/action.c
@@ -0,0 +1,175 @@
+Action()
+{
+
+	web_add_auto_header("Priority", 
+		"u=0, i");
+
+	web_revert_auto_header("Priority");
+
+	web_add_auto_header("DNT", 
+		"1");
+
+	web_add_auto_header("Sec-GPC", 
+		"1");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("77.50.236.214:42030", 
+		"URL=http://77.50.236.214:42030/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/static/js/2.7c253607.chunk.js", ENDITEM, 
+		LAST);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Priority", 
+		"u=4");
+
+	web_add_header("Access-Control-Request-Method", 
+		"GET");
+
+	web_add_header("Access-Control-Request-Headers", 
+		"authorization");
+
+	web_add_auto_header("Origin", 
+		"http://77.50.236.214:42030");
+
+	web_custom_request("bank.wsdl", 
+		"URL=http://77.50.236.203:4879/ws/bank.wsdl", 
+		"Method=OPTIONS", 
+		"TargetFrame=", 
+		"Resource=1", 
+		"Referer=http://77.50.236.214:42030/", 
+		"Snapshot=t2.inf", 
+		LAST);
+
+	web_websocket_send("ID=0", 
+		"Buffer={\"messageType\":\"hello\",\"broadcasts\":{\"remote-settings/monitor_changes\":\"\\\"1723628503818\\\"\"},\"use_webpush\":true}", 
+		"IsBinary=0", 
+		LAST);
+
+	/*Connection ID 0 received buffer WebSocketReceive0*/
+
+	lr_start_transaction("UC07_T01_userLogin");
+
+	web_add_header("Access-Control-Request-Headers", 
+		"content-type");
+
+	web_add_header("Access-Control-Request-Method", 
+		"POST");
+
+	lr_think_time(15);
+
+	web_custom_request("login", 
+		"URL=http://77.50.236.203:4879/login", 
+		"Method=OPTIONS", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"Referer=http://77.50.236.214:42030/", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_auto_header("Priority", 
+		"u=0");
+
+	web_custom_request("login_2", 
+		"URL=http://77.50.236.203:4879/login", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://77.50.236.214:42030/", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		"EncType=application/json", 
+		"Body={\"username\":\"admin@pflb.ru\",\"password\":\"admin\"}", 
+		LAST);
+
+	web_add_cookie("jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBwZmxiLnJ1Iiwicm9sZXMiOlsiYWRtaW4iXSwiaXNzIjoiaHR0cDovLzc3LjUwLjIzNi4yMDM6NDg3OS9sb2dpbiIsImV4cCI6MTcyMzY1Njc1Mn0.g8gkOsd78BMhyB9uhTU30NhBTfXEbT6ovkGk3rRFKr0; DOMAIN=77.50.236.214");
+
+	web_revert_auto_header("Origin");
+
+	web_add_auto_header("Priority", 
+		"u=0, i");
+
+	web_revert_auto_header("Priority");
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_url("77.50.236.214:42030_2", 
+		"URL=http://77.50.236.214:42030/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"Referer=", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/static/js/2.7c253607.chunk.js", ENDITEM, 
+		LAST);
+
+	web_add_auto_header("Origin", 
+		"http://77.50.236.214:42030");
+
+	web_url("bank.wsdl_2", 
+		"URL=http://77.50.236.203:4879/ws/bank.wsdl", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/xml", 
+		"Referer=http://77.50.236.214:42030/", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("UC07_T01_userLogin",LR_AUTO);
+
+	lr_start_transaction("UC07_T02_userSettle");
+
+	web_add_header("Priority", 
+		"u=4");
+
+	web_add_header("Access-Control-Request-Headers", 
+		"authorization");
+
+	web_add_header("Access-Control-Request-Method", 
+		"POST");
+
+	lr_think_time(60);
+
+	web_custom_request("5", 
+		"URL=http://77.50.236.203:4879/house/1/settle/5", 
+		"Method=OPTIONS", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"Referer=http://77.50.236.214:42030/", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_add_header("Priority", 
+		"u=0");
+
+	web_custom_request("5_2", 
+		"URL=http://77.50.236.203:4879/house/1/settle/5", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://77.50.236.214:42030/", 
+		"Snapshot=t8.inf", 
+		"Mode=HTML", 
+		"EncType=", 
+		LAST);
+
+	lr_end_transaction("UC07_T02_userSettle",LR_AUTO);
+
+	return 0;
+}
---
+++ Scripts/UC08_userEvict/data/WebSocketBuffer.h
@@ -0,0 +1,10 @@
+#ifndef VUSER_WEBSOCKET_BUFFER_HEADER
+#define VUSER_WEBSOCKET_BUFFER_HEADER
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+char WebSocketReceive0[] = "{\"messageType\":\"hello\",\"uaid\":\"fa6379dcf3c94d24aa67b0f7577e8633\",\"statu"
+                        "s\":200,\"use_webpush\":true,\"broadcasts\":{}}";
+long WebSocketReceiveLen0   = sizeof(WebSocketReceive0) - 1;	// (record-time: 113 bytes)
+
+#endif
